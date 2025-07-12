62b9d96de427ea676a49855598e52362e63d349d
Rodrigo Antón Moreno <rodri.anton@gmail.com>
2025-07-12 22:52:47 +0200

    feat(content): add helper block to the base template

--- ubroker-web/bp/main.py
+++ ubroker-web/bp/main.py
@@ -4,6 +4,10 @@ from flask import Blueprint, render_template
 bp = Blueprint('bl_main', __name__)
 
 @bp.route('/')
-@bp.route('/index.html')
+@bp.route('/index')
 def index():
     return render_template('index.html', the_title='This is the main page')
+
+@bp.route('/summary')
+def summary():
+    return render_template('summary.html', the_title='This is the main page')
