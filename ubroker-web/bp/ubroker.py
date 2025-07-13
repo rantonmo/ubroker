0beaf2df2fc5997f3ccdb40527cf5fc50c26c5de
Rodrigo Antón Moreno <rodri.anton@gmail.com>
2025-07-13 18:33:08 +0200

    feat(menu): enable menu

--- ubroker-web/bp/ubroker.py
+++ ubroker-web/bp/ubroker.py
@@ -6,12 +6,23 @@ from ..lib.utils import set_menu
 bp = Blueprint('bl_ubroker', __name__)
 
 @bp.route('/')
-@bp.route('/index')
-def index():
-    mc = set_menu('home')
-    return render_template('index.html', mc=mc, the_title='This is the index page')
-
 @bp.route('/summary')
 def summary():
     mc = set_menu('summary')
-    return render_template('summary.html', mc=mc, the_title='This is the summary page', test_var='first session with Fernando')
+    return render_template('summary.html', mc=mc)
+
+@bp.route('/monitors')
+def monitors():
+    mc = set_menu('monitors')
+    return render_template('index.html', mc=mc)
+
+@bp.route('/rules')
+def rules():
+    mc = set_menu('rules')
+    return render_template('rules.html', mc=mc)
+
+
+@bp.route('/status')
+def status():
+    mc = set_menu('status')
+    return render_template('status.html', mc=mc)
